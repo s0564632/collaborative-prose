7b031d4b20483402e7e54ffec00a9661a4ffcdc4
unisono <unisono@systemli.org>
2026-07-13 10:20:42 +0200

    feat: initial setup of collaborative prose project

---
+++ src/main/java/de/htw_berlin/fb4/ossd/s0564632/prose/CollaborativeProse.java
@@ -0,0 +1,19 @@
+package de.htw_berlin.fb4.ossd.s0564632.prose;
+
+// Hier wird die Engine importiert
+import de.htw_berlin.fb4.ossd.prose.builder.ProseBuilder;
+
+public class CollaborativeProse {
+
+    public static void main(String[] args) {
+        // 1. Engine starten
+        ProseBuilder builder = new ProseBuilder();
+
+        // 2. Hier registrierst du später deine Klasse und die der Peers.
+        // Beispiel: builder.register(new MeinSatzAusLab4());
+
+        System.out.println("======================================");
+        System.out.println("Das Projekt-Grundgerüst erstellt!");
+        System.out.println("======================================");
+    }
+}
